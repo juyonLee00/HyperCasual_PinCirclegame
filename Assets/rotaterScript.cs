d4b64dd5d33d5478b2d287fd85f8b59881e8c329
juyonLee00 <ljy012900@gmail.com>
2022-04-29 23:18:30 +0900

    target object

---
+++ Assets/rotaterScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotaterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float rotateSpeed = 50; //회전 속도 설정
+    [SerializeField]
+    private Vector3 rotateAngle = Vector3.forward; //회전 방향 설정
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+      //오브젝트 회전식
+    transform.Rotate(rotateAngle * rotateSpeed * Time.deltaTime);
+    }
+}
