e54e035aee8f7ec871bd71b917c49a4270af8f15
juyonLee00 <ljy012900@gmail.com>
2022-05-11 13:13:48 +0900

    마우스 클릭으로 과녁에 핀 배치
    'Pin' does not contain a definition for 'MoveOneStep' and no accessible extension method 'MoveOneStep' accepting a first argument of type 'Pin' could be found

--- Assets/Pin.cs
+++ Assets/Pin.cs
@@ -6,9 +6,37 @@ public class Pin : MonoBehaviour
 {
     [SerializeField]
     private GameObject square;
+    [SerializeField]
+    private float moveTime = 0.2f;
 
     public void SetInPinStuckToTarget()
     {
+      StopCoroutine("MoveTo");
+
       square.SetActive(true);
+    }
+
+    public void MoveOnStep(float moveDistance)
+    {
+      StartCoroutine("MoveTo", moveDistance);
+    }
+
+    private IEnumerator MoveTo(float moveDistance)
+    {
+      Vector3 start = transform.position;
+      Vector3 end = transform.position + Vector3.up * moveDistance;
+
+      float current = 0;
+      float percent = 0;
+
+      while (percent < 1)
+      {
+        current += Time.deltaTime;
+        percent = current / moveTime;
+
+        transform.position = Vector3.Lerp(start, end, percent);
+
+        yield return null;
+      }
     }
 }
--- Assets/PinSpawner.cs
+++ Assets/PinSpawner.cs
@@ -7,6 +7,8 @@ public class PinSpawner : MonoBehaviour
     // Start is called before the first frame update
     [Header("Commons")]
     [SerializeField]
+    private StageController stageController;
+    [SerializeField]
     private GameObject pinPrefab;
 
     [Header("Stuck Pin")]
@@ -19,9 +21,38 @@ public class PinSpawner : MonoBehaviour
     [SerializeField]
     private float pinLength = 1.5f;
 
+    [Header("Throwable Pin")]
+    [SerializeField]
+    private float bottomAngle = 270;
+    private List<Pin> throwablePins; //던져야 할 핀 오브젝트 리스트
+
+
+    public void Setup()
+    {
+      throwablePins = new List<Pin>();
+    }
+
+
+    private void Update(){
+      if(Input.GetMouseButtonDown(0) && throwablePins.Count > 0)
+      {
+        SetInPinStuckToTarget(throwablePins[0].transform, bottomAngle);
+        throwablePins.RemoveAt(0);
+
+        for (int i = 0; i < throwablePins.Count; ++i)
+        {
+          throwablePins[i].MoveOneStep(stageController.TPinDistance); 
+        }
+      }
+    }
+
     public void SpawnThrowablePin(Vector3 position)
     {
-      Instantiate(pinPrefab, position, Quaternion.identity);
+      GameObject clone = Instantiate(pinPrefab, position, Quaternion.identity);
+
+      Pin pin = clone.GetComponent<Pin>();
+
+      throwablePins.Add(pin);
     }
 
     public void SpawnStuckPin(float angle)
